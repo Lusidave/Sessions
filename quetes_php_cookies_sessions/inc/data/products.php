30f250a46336f3389608ded970639211d7604295
Lusidave <60581269+Lusidave@users.noreply.github.com>
2020-04-27 16:19:09 +0200

    Add files via upload

---
+++ quetes_php_cookies_sessions/inc/data/products.php
@@ -0,0 +1,28 @@
+<?php
+
+
+$catalog = [
+    46 => [
+        'name' => 'Pecan nuts',
+        'description' => 'Cooked by Penny !',
+        'price' => 12.56
+    ],
+    36 => [
+        'name' => 'Chocolate chips',
+        'description' => 'Cooked by Bernadette !',
+        'price' => 11.56
+
+    ],
+    58 => [
+        'name' => 'Full chocolate cookie',
+        'description' => 'Cooked by Bernadette !',
+        'price' => 10.56
+
+    ],
+    32 => [
+        'name' => 'M&M\'s&copy; cookies',
+        'description' => 'Cooked by Penny !',
+        'price' => 12.32
+
+    ],
+];
